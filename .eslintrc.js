cd740541c5d0765e987737ffd71b667c00c90ed1
thaodt0501 <thaodang050196@gmail.com>
2023-06-15 16:59:09 +0700

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,8 +14,6 @@ module.exports = {
     '@typescript-eslint/no-empty-function': ['error', { allow: ['methods', 'arrowFunctions'] }],
     '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/no-explicit-any': ['error'],
-    'no-console': ['error'],
-    'no-debugger': ['error'],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 };
--- src/components/Pages/Login/Login.tsx
+++ src/components/Pages/Login/Login.tsx
@@ -10,7 +10,7 @@ import { ContainerPage } from '../../ContainerPage/ContainerPage';
 
 export function Login() {
   const { errors, loginIn, user } = useStoreWithInitializer(({ login }) => login, dispatchOnCall(initializeLogin()));
-
+  console.log('erro', errors);
   return (
     <div className='auth-page'>
       <ContainerPage>
